25d27d19dba0340cd917a49d26fcffb9223281fc
Siran Cao <siranchao@gmail.com>
2023-06-06 01:26:12 -0400

    complete kudo detail section

--- app/homePage/[id]/page.tsx
+++ app/homePage/[id]/page.tsx
@@ -1,6 +1,6 @@
-
 import KudoCardDetail from "@/app/components/KudoCardDetail";
-
+import Back from "@/app/components/Back";
+import CardDetailTab from "@/app/components/CardDetailTab";
 
 export default async function DisplayKudo({ params }: any) {
     const res: any = await fetch(`http://localhost:8080/api/kudo/oneKudo/${params.id}`, { cache: 'no-store' });
@@ -8,10 +8,12 @@ export default async function DisplayKudo({ params }: any) {
         throw new Error('Failed to fetch data');
     }
     const data: any = await res.json();
-
+    console.log(data.data);
     return (
         <div>
+            <Back />
             <KudoCardDetail kudo={data.data} />
+            <CardDetailTab kudo={data.data} />
         </div>
     )
 }
