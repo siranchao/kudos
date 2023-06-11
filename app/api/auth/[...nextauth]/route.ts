c0de8d6c8fab286744c0b79fcf016f4b1e49e62e
Siran Cao <siranchao@gmail.com>
2023-06-11 00:33:51 -0400

    fix next-auth login token

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,7 @@
 import nextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
+import { signJwtToken } from "@/app/controller/auth";
 
 
 export const authOptions = {
@@ -8,18 +9,54 @@ export const authOptions = {
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID as string,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+        }),
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                username: { label: "Username", type: "text", placeholder: "jsmith" },
+                password: { label: "Password", type: "password" }
+            },
+            
+            async authorize(credentials, req) {
+                const res: any = await fetch(`${process.env.NEXT_PUBLIC_API}/user/auth/login`, {
+                    method: 'POST',
+                    headers: {
+                        'content-type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        email: credentials?.username,
+                        password: credentials?.password,
+                    })
+                })
+                const data = await res.json()
+                
+                if (data) {
+                    console.log('Successfully logged in user')
+                    console.log(data.data)
+                    return data.data
+                } else {
+                    return null
+                }
+            }
         })        
     ],
     secret: process.env.JWT_SECRET,
     callbacks: {
-        async redirect({url, baseUrl}: any) {
-            return '/';
-        },
-        async jwt({token, account}: any) {
-            return {...token, ...account};
+        async jwt({token, user, account}: any) {
+            if (!user?.accessToken && account) {
+                //this is a google account
+                token.accessToken = signJwtToken({
+                    id: user.id,
+                    name: user.name,
+                    email: user.email,
+                })
+            }
+            //console.log("account: ", account);
+            return {...token, ...user};
         },
         async session({session, token}: any) {
-            session.token = token;
+            session.user = token
+            console.log("session is: ", session);
             return session;
         }
     }
